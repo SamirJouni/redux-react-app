0fbc1a439d22df2294db69f165c65b69c9f7fef1
F1r3byte <f1r3byte@yandex.com>
2018-09-22 23:57:07 +0200

    created redux store

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
+import { Provider } from "react-redux";
 import PostForm from "../components/PostForm";
 import Posts from "../components/Posts";
 import "./App.css";
+import { createStore, applyMiddleware } from "redux";
 
+const store = createStore(() => [], {}, applyMiddleware())
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<PostForm />
-        <hr />
-				<Posts />
-			</div>
+			<Provider store={store}>
+				<div className="App">
+					<PostForm />
+					<hr />
+					<Posts />
+				</div>
+			</Provider>
 		);
 	}
 }
