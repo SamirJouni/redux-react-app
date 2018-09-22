31ffa88d80e6078264eea8785e01a0e4d751ad9e
F1r3byte <f1r3byte@yandex.com>
2018-09-22 22:56:30 +0200

    finished the posts component

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,20 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Posts extends Component {
+	constructor() {
+		super();
+		this.state = {
+			posts: []
+		};
+	}
 
-	componentWillMount () {
-		fetch('https://jsonplaceholder.typicode.com/posts')
-		.then( response => response.json())
-		.then(data => console.log(data))
-		.catch(error => console.log(error));
+	componentWillMount() {
+		fetch("https://jsonplaceholder.typicode.com/posts")
+			.then(response => response.json())
+			.then(posts => this.setState({ posts }))
+			.catch(error => console.log(error));
 	}
 
 	render() {
+		const postItems = this.state.posts.map(post =>
+			<div key={post.id}>
+				<h3>{post.title}</h3>
+				<p>{post.body}</p>
+			</div>
+		);
 		return (
 			<React.Fragment>
 				<h1>Posts</h1>
+				{postItems}
 			</React.Fragment>
-		 );
+		);
 	}
 }
 
