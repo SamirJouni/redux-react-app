2b1c86224890150984525295f015fd43bc916e40
F1r3byte <f1r3byte@yandex.com>
2019-02-23 23:17:30 +0100

    Updates

---
+++ precache-manifest.0eee86138569e158df5875763d1d9994.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84375ec8f7db20f49a83",
+    "url": "./static/css/main.44f1498b.chunk.css"
+  },
+  {
+    "revision": "84375ec8f7db20f49a83",
+    "url": "./static/js/main.84375ec8.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "84354471d0d7169549b5",
+    "url": "./static/js/2.84354471.chunk.js"
+  },
+  {
+    "revision": "7cd77ea79b5010a7cf8d48d6910d5db6",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0e63b08e1f14034bb957954e39fe2157.js"
+  "./precache-manifest.0eee86138569e158df5875763d1d9994.js"
 );
 
 workbox.clientsClaim();
